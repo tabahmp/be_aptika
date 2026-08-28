cf1499844b93f5ae555b47fcbf0c3355b7d6fff1
tabahmp <tabahmuhammad2@gmail.com>
2026-08-28 09:18:37 +0700

    fix(migration): populate 7 bidangs sebelum insert bidang_services di migration 000006

--- database/migrations/2026_08_20_000006_add_parent_id_to_services_table.php
+++ database/migrations/2026_08_20_000006_add_parent_id_to_services_table.php
@@ -19,6 +19,30 @@ return new class extends Migration
             }
         });
 
+        // Seed/Update master bidangs (1 s/d 7)
+        $bidangs = [
+            ['id' => 1, 'code' => 'SEKRETARIAT', 'name' => 'Sekretariat', 'description' => 'Sekretariat Diskominfo Jabar'],
+            ['id' => 2, 'code' => 'EGOV',        'name' => 'Bidang E-Government', 'description' => 'Bidang E-Government'],
+            ['id' => 3, 'code' => 'APTIKA',      'name' => 'Bidang Aplikasi Informatika', 'description' => 'Bidang Aplikasi Informatika'],
+            ['id' => 4, 'code' => 'IKP',         'name' => 'Bidang Informasi dan Komunikasi Publik', 'description' => 'Bidang Informasi dan Komunikasi Publik'],
+            ['id' => 5, 'code' => 'PERSANDIAN',  'name' => 'Bidang Persandian dan Keamanan Informasi', 'description' => 'Bidang Persandian dan Keamanan Informasi'],
+            ['id' => 6, 'code' => 'STATISTIK',   'name' => 'Bidang Statistik', 'description' => 'Bidang Statistik'],
+            ['id' => 7, 'code' => 'PLDDIG',      'name' => 'UPTD Pusat Layanan Digital', 'description' => 'UPTD PLDDIG Jawa Barat'],
+        ];
+
+        foreach ($bidangs as $b) {
+            DB::table('bidangs')->updateOrInsert(
+                ['id' => $b['id']],
+                [
+                    'code' => $b['code'],
+                    'name' => $b['name'],
+                    'description' => $b['description'],
+                    'updated_at' => now(),
+                    'created_at' => now(),
+                ]
+            );
+        }
+
         // Seed/Update master services
         $services = [
             ['id' => 1, 'parent_id' => null, 'code' => 'ADMINISTRASI_SURAT', 'name' => 'Administrasi Surat', 'description' => 'Induk Layanan Administrasi Surat'],
